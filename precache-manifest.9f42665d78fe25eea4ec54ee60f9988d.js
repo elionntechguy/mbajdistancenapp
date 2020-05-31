be1930839c8c78c839ad6fb6a2428930269a6f14
elionntechguy <41442942+elionntechguy@users.noreply.github.com>
2020-05-31 23:10:18 +0200

    Updates

---
+++ precache-manifest.9f42665d78fe25eea4ec54ee60f9988d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81fccdfd23fdbd30c4e1bcd0d4351aac",
+    "url": "/mbajdistancenapp/index.html"
+  },
+  {
+    "revision": "a86fa515f10a24951261",
+    "url": "/mbajdistancenapp/static/css/main.31dc35f0.chunk.css"
+  },
+  {
+    "revision": "fbc534cc47b57796d0df",
+    "url": "/mbajdistancenapp/static/js/2.113b49cc.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/mbajdistancenapp/static/js/2.113b49cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a86fa515f10a24951261",
+    "url": "/mbajdistancenapp/static/js/main.6839c3d9.chunk.js"
+  },
+  {
+    "revision": "459c7d283b14e14bae73",
+    "url": "/mbajdistancenapp/static/js/runtime-main.ec83f529.js"
+  }
+]);
